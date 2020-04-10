3f3b6c15e96517eb98c86d851100bb734ad5985b
maulana <maulana@mymindstores.com>
2020-04-10 15:12:07 +0700

    add comment post feature rest

--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,11 @@ Route::get('logout','Api\AuthController@logout');
 Route::post('posts/create','Api\PostController@create')->middleware('jwtAuth');
 Route::post('posts/delete','Api\PostController@delete')->middleware('jwtAuth');
 Route::post('posts/update','Api\PostController@update')->middleware('jwtAuth');
-Route::post('posts','Api\PostController@posts')->middleware('jwtAuth');
+Route::get('posts','Api\PostController@posts')->middleware('jwtAuth');
+
+
+//comment
+Route::post('comment/create','Api\CommentController@create')->middleware('jwtAuth');
+Route::post('comment/delete','Api\CommentController@delete')->middleware('jwtAuth');
+Route::post('comment/update','Api\CommentController@update')->middleware('jwtAuth');
+Route::get('posts/comments','Api\CommentController@comments')->middleware('jwtAuth');
