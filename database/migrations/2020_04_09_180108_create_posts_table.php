2b21f56e6bdf66e3418abb3ce4a0311730a5322c
maulana <maulana@mymindstores.com>
2020-04-11 14:32:47 +0700

    migrate

--- database/migrations/2020_04_09_180108_create_posts_table.php
+++ database/migrations/2020_04_09_180108_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id');
             $table->unsignedBigInteger('user_id');
             $table->string('desc');
             $table->string('photo')->default('');
